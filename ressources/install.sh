45ef55bc97b3e0a4d20c9672cef6300bb99b256f
Maxime RENAUD <74919791+10101349@users.noreply.github.com>
2022-05-16 23:31:00 +0200

    'é'

--- ressources/install.sh
+++ ressources/install.sh
@@ -21,26 +21,26 @@ pip3 install -r requirements.txt
 cd Interlace && python3 ./setup.py install
 cd /opt/app/LinkFinder && python3 ./setup.py install
 
-wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz && tar -xf go1.18.2.linux-amd64.tar.gz
-mkdir -p /usr/local/go/bin && cp ./go/bin/go /usr/local/go/bin/go
-
-
-# install go ressources
-go install github.com/tomnomnom/waybackurls
-go install github.com/tomnomnom/assetfinder@latest
-go install github.com/hakluke/hakrawler@latest
-go install github.com/jaeles-project/gospider@latest
-go install github.com/dwisiswant0/unew@latest
-# GO111MODULE=on go get -u github.com/shenwei356/rush /!\ No need, because: 1 target only by contener
-go install github.com/hiddengearz/jsubfinder@latest
-go install github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-wget https://raw.githubusercontent.com/hiddengearz/jsubfinder/master/.jsf_signatures.yaml && mv .jsf_signatures.yaml ~/.jsf_signatures.yaml
-go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
-go install github.com/lc/gau@latest
-go install github.com/lc/subjs@latest
-
-export HOME=/opt/app/
-export GOPATH=$HOME/go/bin
-export PATH=$PATH:$GOPATH
-export OUTPUT_DIR=/opt/app
+#wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz && tar -xf go1.18.2.linux-amd64.tar.gz
+#mkdir -p /usr/local/go/bin && cp ./go/bin/go /usr/local/go/bin/go
+#
+#
+## install go ressources
+#go install github.com/tomnomnom/waybackurls
+#go install github.com/tomnomnom/assetfinder@latest
+#go install github.com/hakluke/hakrawler@latest
+#go install github.com/jaeles-project/gospider@latest
+#go install github.com/dwisiswant0/unew@latest
+## GO111MODULE=on go get -u github.com/shenwei356/rush /!\ No need, because: 1 target only by contener
+#go install github.com/hiddengearz/jsubfinder@latest
+#go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+#go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+#wget https://raw.githubusercontent.com/hiddengearz/jsubfinder/master/.jsf_signatures.yaml && mv .jsf_signatures.yaml ~/.jsf_signatures.yaml
+#go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+#go install github.com/lc/gau@latest
+#go install github.com/lc/subjs@latest
+#
+#export HOME=/opt/app/
+#export GOPATH=$HOME/go/bin
+#export PATH=$PATH:$GOPATH
+#export OUTPUT_DIR=/opt/app
