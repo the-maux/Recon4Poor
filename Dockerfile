a98ff5af5f97eed66cee1dc6ec7c91ca164a3387
Maxime RENAUD <74919791+10101349@users.noreply.github.com>
2022-06-08 19:09:52 +0200

    testing python,

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY . /opt/app
 
 WORKDIR /opt/app
 
+ENV PYTHONPATH=$PWD
+
 RUN bash ./ressources/install.sh
 
 CMD ["python3", "./src/main.py"]
--- ressources/install.sh
+++ ressources/install.sh
@@ -5,14 +5,10 @@ apt -yqq install python3 python3-pip wget git unzip nano iputils-ping &> nooutpu
 apt-get clean &> nooutput
 
 ## install python ressources
-git clone https://github.com/nsonaniya2010/SubDomainizer.git
-git clone https://github.com/aboul3la/Sublist3r.git
-git clone https://github.com/duty1g/subcat
+git clone https://github.com/nsonaniya2010/SubDomainizer.git pip3 install -r SubDomainizer/requirements.txt &> nooutput
+git clone https://github.com/aboul3la/Sublist3r.git && pip3 install -r Sublist3r/requirements.txt &> nooutput
+git clone https://github.com/duty1g/subcat && pip3 install -r subcat/requirements.txt &> nooutput
 #git clone https://github.com/dark-warlord14/LinkFinder.git
-
-pip3 install -r subcat/requirements.txt &> nooutput
-pip3 install -r Sublist3r/requirements.txt &> nooutput
-pip3 install -r SubDomainizer/requirements.txt &> nooutput
 # cd /opt/app/LinkFinder && python3 ./setup.py install
 
 
@@ -40,7 +36,7 @@ wget -q https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetf
 
 export HOME=/opt/app/
 export GOPATH=$HOME/go/bin
-export PYTHONPATH="/opt/app"
+export PYTHONPATH=$HOME
 export PATH=/usr/local/go/bin:$GOPATH:$PATH
 
 rm -vf go1.18.3.linux-amd64* Dockerfile Release nooutput  # TODO: cleaning is shitty
