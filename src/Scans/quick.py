ba4ad0c57cb15e4647323e45ea0ecb0a8f784f70
Maxime RENAUD <74919791+10101349@users.noreply.github.com>
2022-06-08 16:56:16 +0200

    testing python,

--- src/Scans/quick.py
+++ src/Scans/quick.py
@@ -15,6 +15,8 @@ def exec_tools(cmd, usefFile=True):
 
 def use_python_tools(target):
     """ Use python script subcat & sublist3r & SubDomainzer """
+    start_python = time.time()
+
     cmd = f'echo {target} | python3 subcat/subcat.py -silent'
     subcat_res = exec_tools(cmd=cmd, usefFile=False)
     print(f"(DEBUG) Subcat found: {len(subcat_res)} domain(s) in scope")
@@ -27,11 +29,15 @@ def use_python_tools(target):
     subDomainizer_res = exec_tools(cmd=cmd, usefFile=True)
     print(f'(DEBUG) SubDomainizer found: {len(subDomainizer_res)} domain(s) in scope')
 
-    return extract_subdomains(subcat_res + sublist3r_res + subDomainizer_res)
+    end_python = time.time()
+    python_results = extract_subdomains(subcat_res + sublist3r_res + subDomainizer_res)
+    print(f'(DEBUG) PYTHON TOOLS FOUND {len(python_results)} SUBDOMAIN in {end_python - start_python} seconds ! \n')
+    return python_results
 
 
 def use_go_tools(target):
     """ User Go binary waybackurls & gau & subfinder """
+    start = time.time()
     wayback_urls = exec_tools(cmd=f'echo "{target}" | waybackurls', usefFile=False)
     print(f'(DEBUG) Waybackurls found {len(wayback_urls)} endpoints')
 
@@ -42,37 +48,34 @@ def use_go_tools(target):
     print(f'(DEBUG) subfinder found {len(subfinder)} endpoints')
 
     subdomains = extract_subdomains(wayback_urls + gau_urls + subfinder)
+    end_go = time.time()
+    print(f'(DEBUG) GO TOOLS FOUND {len(subdomains)} SUBDOMAIN in {end_go - start} seconds !\n')
     return subdomains
 
 
 def quick_scan(target):
-    start = time.time()
-    go_results = use_go_tools(target)
-    end_go = time.time()
-    print(f'(DEBUG) GO TOOLS FOUND {len(go_results)} SUBDOMAIN in {end_go - start} seconds !\n')
+    # start = time.time()
+    # go_results = use_go_tools(target)
+    # python_results = use_python_tools(target)
+    # final_res = extract_subdomains(go_results + python_results)
+    # end = time.time()
+    # print(f'(DEBUG) ALL TOOLS FOUND {len(final_res)} SUBDOMAIN in {end - start} seconds ! ')
+    # return final_res
+    pThread = list()
+    pThread.append(Thread(target=use_go_tools, args=(target)))
+    pThread.append(Thread(target=use_python_tools, args=(target)))
+    [process.start() for process in pThread]
+    [process.join() for process in pThread]
+    return list()
 
-    start_python = time.time()
-    python_results = use_python_tools(target)
-    end_python = time.time()
-    print(f'(DEBUG) PYTHON TOOLS FOUND {len(go_results)} SUBDOMAIN in {end_python - start_python} seconds ! \n')
-
-    final_res = extract_subdomains(go_results + python_results)
-    print(f'(DEBUG) ALL TOOLS FOUND {len(final_res)} SUBDOMAIN in {end_python - start_python} seconds ! ')
-    return final_res
-
-
-# def quick_scan_parrall(target): # ca dump dans des fichiers, mais il faut le recup in memory mais cest en thread :/
-#     listOfToolsToExec = list()
-#     p1 = Thread(target=use_python_tool, args=('./tools/SubDomainizer/', 'SubDomainizer.py', ' -l target.txt -o SubDomainizer.txt -san all  &> nooutput'))
-#     p2 = Thread(target=use_python_tool, args=('subfinder', f'-d {target} -silent'))
-#     p3 = Thread(target=use_python_tool, args=('./tools/Sublist3r/', 'sublist3r.py', f'-d {target} -o sublist3r.txt &> nooutput'))
-#     p4 = Thread(target=use_assetfinder, args=(target))
-#     listOfToolsToExec.append(p1)
-#     listOfToolsToExec.append(p2)
-#     listOfToolsToExec.append(p3)
-#     listOfToolsToExec.append(p4)
-#     [process.start() for process in listOfToolsToExec]
-#     [process.join() for process in listOfToolsToExec]
+def quick_scan_parrall(target): # ca dump dans des fichiers, mais il faut le recup in memory mais cest en thread :/
+    listOfToolsToExec = list()
+    p1 = Thread(target=use_go_tools, args=(target))
+    p2 = Thread(target=use_python_tools, args=(target))
+    listOfToolsToExec.append(p1)
+    listOfToolsToExec.append(p2)
+    [process.start() for process in listOfToolsToExec]
+    [process.join() for process in listOfToolsToExec]
 
 
 # domains_found_assetfinder = use_assetfinder(target)
