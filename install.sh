cc5e11d59dd27fe3fd9c45f504942fa2df06eefa
Maxime RENAUD <74919791+10101349@users.noreply.github.com>
2022-06-09 05:35:54 +0200

    testing python,

--- install.sh
+++ install.sh
@@ -13,27 +13,26 @@ git clone https://github.com/GerbenJavado/LinkFinder.git && cd ./LinkFinder && p
 
 # Recoon Go tool
 wget -q https://go.dev/dl/go1.18.3.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz &&
-  ln -s /usr/local/go/bin/go /usr/bin/go
-
+  ln -s /usr/local/go/bin/go /usr/bin/go &> nooutput
 
 wget -q https://github.com/projectdiscovery/subfinder/releases/download/v2.5.2/subfinder_2.5.2_linux_amd64.zip &&
-  unzip subfinder_2.5.2_linux_amd64.zip && mv ./subfinder /usr/bin/subfinder
-
-wget -q https://github.com/lc/gau/releases/download/v2.1.1/gau_2.1.1_linux_386.tar.gz &&
-  tar -xvf gau_2.1.1_linux_386.tar.gz && mv ./gau /usr/bin/gau
+  unzip subfinder_2.5.2_linux_amd64.zip && mv subfinder /usr/bin/subfinder &> nooutput
 
 wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.2.1/httpx_1.2.1_linux_amd64.zip &&
-  unzip httpx_1.2.1_linux_amd64.zip && mv ./httpx /usr/bin/httpx
+  unzip httpx_1.2.1_linux_amd64.zip && mv httpx /usr/bin/httpx
+
+wget -q wget https://github.com/jaeles-project/gospider/releases/download/v1.1.6/gospider_v1.1.6_linux_x86_64.zip &&
+  unzip gospider_v1.1.6_linux_x86_64.zip && mv gospider_v1.1.6_linux_x86_64/gospider /usr/bin/gospider &> nooutput
+
+wget -q https://github.com/lc/gau/releases/download/v2.1.1/gau_2.1.1_linux_386.tar.gz &&
+  tar -xf gau_2.1.1_linux_386.tar.gz && mv gau /usr/bin/gau &> nooutput
 
 # go install github.com/tomnomnom/assetfinder@latest
 wget -q https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz &&
-  tar -xvf assetfinder-linux-amd64-0.1.1.tgz && mv ./assetfinder /usr/bin/assetfinder
-
-wget -q wget https://github.com/jaeles-project/gospider/releases/download/v1.1.6/gospider_v1.1.6_linux_x86_64.zip &&
-  unzip gospider_v1.1.6_linux_x86_64.zip && mv gospider_v1.1.6_linux_x86_64/gospider /usr/bin/gospider
+  tar -xf assetfinder-linux-amd64-0.1.1.tgz && mv assetfinder /usr/bin/assetfinder &> nooutput
 
 wget -q https://github.com/lc/subjs/releases/download/v1.0.1/subjs_1.0.1_linux_amd64.tar.gz &&
-  tar -xvf subjs_1.0.1_linux_amd64.tar.gz && mv ./subjs  /usr/bin/subjs &> nooutput
+  tar -xf subjs_1.0.1_linux_amd64.tar.gz && mv subjs  /usr/bin/subjs &> nooutput
 
 # TO-FIX: because of https://github.com/tomnomnom/waybackurls/issues/41
 git clone https://github.com/tomnomnom/waybackurls && # go install github.com/tomnomnom/waybackurls@latest
@@ -47,4 +46,4 @@ git clone https://github.com/hakluke/hakrawler &&
   cd hakrawler && go build  && ln -s /opt/app/hakrawler/hakrawler /usr/bin/hakrawler &> nooutput && cd -
 
 
-rm -vf *.zip *.gz* *.md *.tgz Dockerfile Release nooutput
+rm -f *.zip *.gz* *.md *.tgz Dockerfile Release nooutput
