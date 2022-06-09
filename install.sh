24ddc34ce35cfd0377518e938528b94c03c9aca9
Maxime RENAUD <74919791+10101349@users.noreply.github.com>
2022-06-09 03:16:18 +0200

    testing python,

--- install.sh
+++ install.sh
@@ -11,13 +11,9 @@ git clone https://github.com/duty1g/subcat && pip3 install -r subcat/requirement
 #git clone https://github.com/dark-warlord14/LinkFinder.git
 # cd /opt/app/LinkFinder && python3 ./setup.py install
 
-
+# Recoon Go tool
 wget -q https://go.dev/dl/go1.18.3.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz &&
   ln -s /usr/local/go/bin/go /usr/bin/go
-
-# TO-FIX: because of https://github.com/tomnomnom/waybackurls/issues/41
-git clone https://github.com/tomnomnom/waybackurls.git && # go install github.com/tomnomnom/waybackurls@latest
-  cd waybackurls && go build main.go && ln -s /opt/app/waybackurls/main /usr/bin/waybackurls &> nooutput
 
 # go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 wget -q https://github.com/projectdiscovery/subfinder/releases/download/v2.5.2/subfinder_2.5.2_linux_amd64.zip &&
@@ -35,15 +31,22 @@ wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.2.1/httpx
 wget -q https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz &&
   tar -xvf assetfinder-linux-amd64-0.1.1.tgz && mv ./assetfinder /usr/bin/assetfinder
 
+# TO-FIX: because of https://github.com/tomnomnom/waybackurls/issues/41
+git clone https://github.com/tomnomnom/waybackurls.git && # go install github.com/tomnomnom/waybackurls@latest
+  cd waybackurls && go build main.go && ln -s /opt/app/waybackurls/main /usr/bin/waybackurls &> nooutput && cd -
+
+# Js files finder Go tool
+git clone https://github.com/ThreatUnkown/jsubfinder.git &&
+  cd jsubfinder && go build main.go && ln -s /opt/app/jsubfinder/main /usr/bin/jsubfinder &> nooutput && cd -
+
+#go install github.com/hakluke/hakrawler@latest
+git clone https://github.com/hakluke/hakrawler &&
+  cd hakrawler && go build  && ln -s /opt/app/hakrawler/hakrawler /usr/bin/hakrawler &> nooutput && cd -
 
 rm -vf go1.18.3.linux-amd64* Dockerfile Release nooutput  # TODO: cleaning is shitty
 
-#go install github.com/hakluke/hakrawler@latest
 #go install github.com/jaeles-project/gospider@latest
-#go install github.com/lc/subjs@latest
-#go install github.com/hiddengearz/jsubfinder@latest
-#go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
-#go install github.com/dwisiswant0/unew@latest
+go install github.com/lc/subjs@latest
 
 #cat ./SecretFinder/requirements.txt | grep -v "requests" >> requirement_all.txt
 #git clone https://github.com/m4ll0k/SecretFinder.git
