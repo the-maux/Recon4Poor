ccd716c8a19f109e822afbeac212cf5b8fa4d16b
Maxime RENAUD <74919791+10101349@users.noreply.github.com>
2022-06-11 01:26:05 +0200

    testing python,

--- src/Scans/regular.py
+++ src/Scans/regular.py
@@ -1,14 +1,25 @@
 from src.Scans.quick import quick_scan
 from threading import Thread
+from src.Utils.Shell import shell
+
+
+def exec_pool_domains(pool_threads):
+    [process.start() for process in pool_threads]
+    [process.join() for process in pool_threads]
 
 
 def regular_scan(domains):
     print(f'(DBUG) Starting regular scan with {len(domains)} domains')
     results = list()
+    rcx = 0
+    rdx = 0
     pThreads = list()
     for domain in domains:
         pThreads.append(Thread(target=quick_scan, args=(domain,)))
-    [process.start() for process in pThreads]
-    [process.join() for process in pThreads]
-    stdout, stderr, returncode = shell('cat tmp-search.txt', verbose=False)
+        rcx = rcx + 1
+        if rcx == 5:
+            print(f'(DEBUG) Starting pools of domains: {rcx}')
+            exec_pool_domains(pThreads)
+            pThreads = list()
+            rcx = 0
     return results
