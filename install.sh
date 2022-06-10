cbf11fc893972a33fc07ef4602c4b6dcca4e40a8
Maxime RENAUD <74919791+10101349@users.noreply.github.com>
2022-06-10 12:05:47 +0200

    testing python,

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 apt -yqq update &> nooutput && apt -yqq install python3 python3-pip wget git unzip nano iputils-ping &> nooutput
-apt-get clean &> nooutput
+apt-get clean && ln -s /usr/bin/python3 /usr/bin/python &> nooutput
 
-## install python ressources for recon
+
+## Install python ressources for recon
 git clone https://github.com/nsonaniya2010/SubDomainizer.git && pip3 install -r SubDomainizer/requirements.txt &> nooutput
 git clone https://github.com/aboul3la/Sublist3r.git && pip3 install -r Sublist3r/requirements.txt &> nooutput
 git clone https://github.com/duty1g/subcat && pip3 install -r subcat/requirements.txt &> nooutput
-## install python ressources Analyze files
-git clone https://github.com/m4ll0k/SecretFinder.git && cd SecretFinder && pip3 install -r requirements.txt && cd -
-git clone https://github.com/GerbenJavado/LinkFinder.git && cd ./LinkFinder && python3 setup.py install && cd -
+
+## Install python ressources Analyze files
+git clone https://github.com/m4ll0k/SecretFinder.git && cd SecretFinder && pip3 install -r requirements.txt && cd - &> nooutput
+git clone https://github.com/GerbenJavado/LinkFinder.git && cd ./LinkFinder && python3 setup.py install && cd - &> nooutput
 
 # Recoon Go tool
 wget -q https://go.dev/dl/go1.18.3.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz &&
@@ -19,7 +21,7 @@ wget -q https://github.com/projectdiscovery/subfinder/releases/download/v2.5.2/s
   unzip subfinder_2.5.2_linux_amd64.zip && mv subfinder /usr/bin/subfinder &> nooutput
 
 wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.2.1/httpx_1.2.1_linux_amd64.zip &&
-  unzip httpx_1.2.1_linux_amd64.zip && mv httpx /usr/bin/httpx
+  unzip httpx_1.2.1_linux_amd64.zip && mv httpx /usr/bin/httpx &> nooutput
 
 wget -q wget https://github.com/jaeles-project/gospider/releases/download/v1.1.6/gospider_v1.1.6_linux_x86_64.zip &&
   unzip gospider_v1.1.6_linux_x86_64.zip && mv gospider_v1.1.6_linux_x86_64/gospider /usr/bin/gospider &> nooutput
@@ -36,14 +38,14 @@ wget -q https://github.com/lc/subjs/releases/download/v1.0.1/subjs_1.0.1_linux_a
 
 # TO-FIX: because of https://github.com/tomnomnom/waybackurls/issues/41
 git clone https://github.com/tomnomnom/waybackurls && # go install github.com/tomnomnom/waybackurls@latest
-  cd waybackurls && go build main.go && ln -s /opt/app/waybackurls/main /usr/bin/waybackurls &> nooutput && cd -
+  cd waybackurls && go build main.go && ln -s /opt/app/waybackurls/main /usr/bin/waybackurls && cd - &> nooutput
 
 git clone https://github.com/ThreatUnkown/jsubfinder &&
-  cd jsubfinder && go build main.go && ln -s /opt/app/jsubfinder/main /usr/bin/jsubfinder &> nooutput && cd -
+  cd jsubfinder && go build main.go && ln -s /opt/app/jsubfinder/main /usr/bin/jsubfinder && cd - &> nooutput
 
 #go install github.com/hakluke/hakrawler@latest
 git clone https://github.com/hakluke/hakrawler &&
-  cd hakrawler && go build  && ln -s /opt/app/hakrawler/hakrawler /usr/bin/hakrawler &> nooutput && cd -
+  cd hakrawler && go build  && ln -s /opt/app/hakrawler/hakrawler /usr/bin/hakrawler && cd - &> nooutput
 
 
 rm -f *.zip *.gz* *.md *.tgz Dockerfile Release nooutput
