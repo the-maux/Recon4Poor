48d521013b8b9e54e2e4f89e3bfe64b930ffd4a9
10101349 <G0I63Rs>
2022-06-25 03:44:34 +0200

    fixing #6 ?

--- src/unit_test.py
+++ src/unit_test.py
@@ -1,10 +1,12 @@
 import unittest, sys, os
+
+from src import coverage
 from src.Utils.Shell import shell
 from src.main import B4DID34T
 
 
 def check_binary_access(cmd):
-    stdo, stde, status = shell(cmd, verbose=True)
+    stdo, stde, status = shell(cmd, verbose=False)
     return status == 0 or status == 2  # 3 for hakrawler
 
 
@@ -28,15 +30,61 @@ class UnitTests(unittest.TestCase):
         self.assertTrue(check_binary_access(cmd='jsubfinder -h'))
         self.assertTrue(check_binary_access(cmd='hakrawler -h'))
         self.assertTrue(check_binary_access(cmd='gospider -h'))
-        self.assertTrue(check_binary_access(cmd='gospider -h'))
         self.assertTrue(check_binary_access(cmd='subjs -h'))
+
+        self.assertTrue(check_binary_access(cmd='amass -h'))
+        self.assertTrue(check_binary_access(cmd='gobuster -h'))
+        self.assertTrue(check_binary_access(cmd='shuffledns -h'))
+        self.assertTrue(check_binary_access(cmd='dmut -h'))
+        self.assertTrue(check_binary_access(cmd='dnsx -h'))
+#        self.assertTrue(check_binary_access(cmd='dnscan -h'))
+#        self.assertTrue(check_binary_access(cmd='puredns -h'))
+
+        # TOTEST
+        # https://github.com/OWASP/Amass
+        # https://github.com/projectdiscovery/dnsx a use ?
+        # https://github.com/projectdiscovery/shuffledns
+        # https://github.com/d3mondev/puredns
+        # https://github.com/bp0lr/dmut moe
+        # https://github.com/OJ/gobuster
+        ### https://github.com/codingo/DNSCewl + https://github.com/ProjectAnte/dnsgen ??????
+        # https://github.com/rbsec/dnscan python
 
     @classmethod
     def tearDownClass(cls):
         pass  # clean ce qui faut
 
 
+def coverage_me():
+    conf = """ 
+[run]
+omit =
+    src/unit_test.py
+[report]
+exclude_lines =
+    pragma: no cover
+    if __name__ == .__main__.:
+"""
+    shell(cmd=f'echo -n {conf} > .coverage')
+    shell('/home/app/.local/bin/coverage run --source="." --rcfile=.coverage -m unittest src/unit_test.py')
+    stdout, stderr, returncode = shell('/home/app/.local/bin/coverage report')
+    print(stdout)
+    try:
+        pourcentage = int(stdout.split('\n')[-2].strip().split(' ')[-1].replace("%", ""))
+    except ValueError as e:
+        pourcentage = -1
+        print(f'(ERROR) In coverage, cant analyse result {e}')
+    if pourcentage < 40:
+        print(f"(DEBUG) Pourcentage de coverage actuel [{pourcentage}]en dessous de 40% ><' !!! ")
+        exit(-1)
+    print('(DEBUG) Coverage checking OK !')
+    exit(0)
+
+
 if __name__ == '__main__':
+    coverage = False
+    if coverage:
+        coverage_me()
     sys.path.insert(0, os.getcwd())
     print(f'Tests bypass le temps que la C.I est construite sur les multi Dockers')
     unittest.main(verbosity=2)
